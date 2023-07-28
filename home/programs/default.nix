7b142058f0d34277e022b399a3902acd949ab29b
Zoellner122 <r1997@live.nl>
2023-07-28 12:54:52 +0200

    Adding program list

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,5 +1,33 @@
 { config, pkgs, ... }:
 
 {
-  
+  commonPrograms = [
+
+    # Default tools I use
+    wget
+    git
+    net-tools
+    vscodium
+    via
+    clamav
+    alacritty
+
+    # Web browsers
+    brave
+    firefox #fallback
+
+    # Communication
+    discord
+  ];
+
+  normandyPrograms = [
+    mattermost
+    _1password
+  ];
+
+  tempestPrograms = [
+    lutris
+    steam
+    wine-wayland
+  ]
 }
